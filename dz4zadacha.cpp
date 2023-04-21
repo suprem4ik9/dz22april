892e84d805ddc4bb30fb806f8b5abff1bb2325bb
suprem4ik9 <116392677+suprem4ik9@users.noreply.github.com>
2023-04-22 03:18:25 +0500

    Add files via upload

---
+++ dz4zadacha.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <cstdio>
+
+using namespace std;
+
+string str;
+vector<vector<long long> > F;
+int n;
+void input()
+{
+    cin >> str;
+    n = str.size();
+    F = vector<vector<long long> >(n, vector<long long>(n, 0));
+}
+void solve()
+{
+    int j;
+    for (int k = 0; k < n; k++)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            j = i + k;
+            if (j == n)
+                break;
+            if (i == j)
+                F[i][j] = 1;
+            else
+            {
+                if (str[i] != str[j])
+                    F[i][j] = F[i + 1][j] + F[i][j - 1] - F[i + 1][j - 1];
+                else
+                    F[i][j] = F[i + 1][j] + F[i][j - 1] + 1;
+            }
+        }
+    }
+    cout << F[0].back();
+}
+int main()
+{
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    input();
+    solve();
+    return 0;
+}
---
+++ dz5zadacha.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <vector>
+#include <limits.h>
+
+using namespace std;
+
+vector<int> P, W;
+int n;
+int E, F;
+vector<int> MAX, MIN;
+void input()
+{
+    cin >> E >> F;
+    cin >> n;
+    MAX = vector<int>(F - E + 1, INT_MIN);
+    MIN = vector<int>(F - E + 1, INT_MAX);
+    P.resize(n); W.resize(n);
+    for (int i = 0; i < n; i++)
+        cin >> P[i] >> W[i];
+}
+void solve()
+{
+    MIN[0] = 0;
+    MAX[0] = 0;
+    for (int i = 0; i < MIN.size(); i++)
+    {
+        for (int j = 0; j < W.size(); j++)
+        {
+            if (i - W[j] >= 0 && MIN[i - W[j]] != INT_MAX)
+            {
+                MIN[i] = min(MIN[i], MIN[i - W[j]] + P[j]);
+                MAX[i] = max(MAX[i], MAX[i - W[j]] + P[j]);
+            }
+        }
+    }
+
+}
+void output()
+{
+    if (MIN.back() == INT_MAX)
+        cout << "This is impossible.";
+    else
+        cout << MIN.back() << ' ' << MAX.back();
+}
+int main()
+{
+    input();
+    solve();
+    output();
+
+    return 0;
+}
